cca795de9fe9cc83c6f5735a02d21666484f8eed
ClementineLavender <natalibulavina78@gmail.com>
2026-06-26 05:12:51 +0200

    update timer interface

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,3 +3,44 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from "izitoast";
 import 'izitoast/dist/css/iziToast.min.css';
 
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates){
+    userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+    if (userSelectedDate <= new Date()){
+      iziToast.error({
+        title: 'Error',
+        message: '"Please choose a date in the future"',
+        position: 'topRight',
+      })
+      startBtn.disabled = true;
+    }else {
+      startBtn.disabled = false;
+    }
+  }
+}
+flatpickr (input, options);
+
+function addLeadingZero (value){
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerInterface({ days, hours, minutes, seconds }){
+  daysEl.textContent = addLeadindZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondEl.textContent = addLeadingZero(seconds);
+}
